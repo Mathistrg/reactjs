2752172828d8ec5400968f2438e6c94acef6c404
Mathistrg <123164665+Mathistrg@users.noreply.github.com>
2023-12-11 14:22:09 +0100

    euh jsp

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Counter from "@/components/Counter"
+import Welcome from "@/components/Welcome"
+
+
+export default function Home () {
+    return (
+      <main>
+        <Welcome name = "François"/>
+        <Counter/>
+      </main>
+    );
+}
